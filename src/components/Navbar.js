21e7088fd4cf1ae15af72f0542df083f53f4203d
Marcos Scalabrino <marscalabrino@hotmail.com>
2022-03-29 23:25:29 -0300

    commit navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    return (
+        <nav className="App navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Logo</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <a className="nav-link" href="#">Products</a>
+                    <a className="nav-link" href="#">Pricing</a>
+                    {/* <a className="nav-link disabled">Disabled</a> */}
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
